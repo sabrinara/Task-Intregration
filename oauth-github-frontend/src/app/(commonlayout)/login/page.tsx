ec7a2f190344840d66cf8a9d49fa35e17b8a351f
TechBuilderTeam <saashid18@gmail.com>
2024-10-23 14:56:10 +0600

    jira login done

--- oauth-github-frontend/src/app/(commonlayout)/login/page.tsx
+++ oauth-github-frontend/src/app/(commonlayout)/login/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { loginUser } from "../../../utils/actions/loginUser";
-
 import { toast } from "sonner";
 import { useEffect } from "react";
 import { useRouter } from "next/navigation";
@@ -13,9 +12,10 @@ export interface UserType {
   githubRepo: string;
   trelloBoard: string;
 }
+
 const LoginPage = () => {
   const { data: session } = useSession();
-  const route = useRouter();
+  const router = useRouter();
 
   useEffect(() => {
     if (session && session.user?.login) {
@@ -26,7 +26,7 @@ const LoginPage = () => {
         githubRepo: "githubRepoExample",
         trelloBoard: "trelloBoardExample",
       };
-  
+
       loginUser(userData as UserType)
         .then((userInfo) => {
           if (userInfo.access_token) {
@@ -39,6 +39,7 @@ const LoginPage = () => {
         })
         .catch((error) => {
           console.error("Login error:", error);
+          toast.error("Login failed. Please try again.");
         });
     } else {
       localStorage.removeItem("access_token");
@@ -47,16 +48,15 @@ const LoginPage = () => {
       localStorage.removeItem("provider");
     }
   }, [session]);
-  
 
   const handleRepo = () => {
-    route.push("/repository");
+    router.push("/repository");
   };
 
   return (
     <div className="my-10">
       <h1 className="text-center text-4xl mb-5">
-        Integrate with <span className="text-accent">GitHub</span> or <span className="text-accent">Slack</span>
+        Integrate with <span className="text-accent">GitHub</span>, <span className="text-accent">Slack</span>, or <span className="text-accent">Jira</span>
       </h1>
       <div className="gap-4 mt-10">
         <div className="w-[30%] h-[80%] mx-auto rounded-xl">
@@ -67,31 +67,41 @@ const LoginPage = () => {
                   Disconnect
                 </button>
 
-               {session.user?.provider === "github" && (
+                {session.user?.provider === "github" && (
                   <button className="py-4 text-xl bg-sky-50 px-4 rounded-xl" onClick={handleRepo}>
-                  See All Repositories
-                </button>
-               )}
-               {session.user?.provider === "slack" && (
+                    See All Repositories
+                  </button>
+                )}
+                {session.user?.provider === "slack" && (
                   <button className="py-4 text-xl bg-green-50 px-4 rounded-xl" onClick={handleRepo}>
-                  See All Boards
-                </button>
-               )}
-                           
-                </div>
+                    See All Boards
+                  </button>
+                )}
+                {session.user?.provider === "atlassian" && (
+                  <button className="py-4 text-xl bg-orange-50 px-4 rounded-xl" onClick={handleRepo}>
+                    See All Jira Boards
+                  </button>
+                )}
+              </div>
             ) : (
               <div className="flex flex-col gap-4">
                 <button
                   className="py-4 text-xl bg-sky-50 px-4 rounded-xl"
-                  onClick={() => signIn("github", { callbackUrl: "http://localhost:3000" })}
+                  onClick={() => signIn("github", { callbackUrl: process.env.NEXT_PUBLIC_CALLBACK_URL })}
                 >
                   GitHub
                 </button>
                 <button
                   className="py-4 text-xl bg-green-50 px-4 rounded-xl"
-                  onClick={() => signIn("slack", { callbackUrl: "https://oauth-github-frontend.vercel.app" })}
+                  onClick={() => signIn("slack", { callbackUrl: process.env.NEXT_PUBLIC_CALLBACK_URL })}
                 >
                   Slack
+                </button>
+                <button
+                  className="py-4 text-xl bg-orange-50 px-4 rounded-xl"
+                  onClick={() => signIn("atlassian", { callbackUrl:`http://localhost:3000/api/auth/callback/atlassian` })}
+                >
+                  Jira
                 </button>
               </div>
             )}
--- oauth-github-frontend/src/utils/authOptions.ts
+++ oauth-github-frontend/src/utils/authOptions.ts
@@ -1,5 +1,6 @@
 import { NextAuthOptions } from "next-auth";
 import GithubProvider from "next-auth/providers/github";
+import AtlassianProvider from "next-auth/providers/atlassian";
 import SlackProvider from "next-auth/providers/slack";
 
 export const authOptions: NextAuthOptions = {
@@ -12,6 +13,15 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.SLACK_CLIENT_ID as string,
       clientSecret: process.env.SLACK_CLIENT_SECRET as string,
     }),
+    AtlassianProvider({
+      clientId: process.env.ATLASSIAN_CLIENT_ID as string,
+      clientSecret: process.env.ATLASSIAN_CLIENT_SECRET as string,
+      authorization: {
+        params: {
+          scope: "write:jira-work read:jira-work read:jira-user offline_access read:me"
+        }
+      }
+    })
   ],
   pages: {
     signIn: '/login',
@@ -38,6 +48,10 @@ export const authOptions: NextAuthOptions = {
       }
     
       if (account?.provider === "slack" && profile?.email) {
+        token.login = profile.email;
+      }
+
+      if (account?.provider === "atlassian" && profile?.email) {
         token.login = profile.email;
       }
       
